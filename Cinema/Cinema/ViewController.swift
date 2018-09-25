7889509d2ce43000918da0c61c41de25dd447853
ChinhDat <9dat102@gmail.com>
2018-09-25 09:00:02 +0700

    Khoi tao du an

---
+++ Cinema/Cinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by ChinhDat on 9/25/18.
+//  Copyright © 2018 ChinhDat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
